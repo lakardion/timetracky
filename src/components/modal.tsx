a955c2c5e748680a34bf3baa6a3955da391321b9
lakardion <lakardion@protonmail.com>
2022-07-20 13:24:16 -0300

    fix(modal) 🐛 fix issue with firefox and modals - Rework modal to detect firefox and render an opacity backdrop instead of a blur backdrop (chrome)

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,6 +1,10 @@
 import { FC, ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
+// for some reason firefox does not support backdrop blur
+const agent = window.navigator.userAgent;
+const isFirefox = agent.indexOf("Firefox") !== -1;
+
 /**
  * Do not call this component until the dom has rendered fully, else we'd get an error with document not being found
  * @param param0
@@ -9,29 +13,29 @@ import { createPortal } from "react-dom";
 export const Modal: FC<{
   onClose: () => void;
   children: ReactNode;
-}> = ({ onClose, children }) => {
-  const [fade, setFade] = useState(false);
+  className?: string;
+}> = ({ onClose, className = "", children }) => {
+  const [hasFaded, setHasFaded] = useState(false);
 
   useEffect(() => {
-    setFade(true);
+    setHasFaded(true);
   }, []);
 
-  const opacityValue = fade ? "opacity-100" : "opacity-0";
+  const opacityValue = hasFaded ? "opacity-100" : "opacity-0";
+  const blurOrBackdrop = isFirefox
+    ? "bg-gray-300/50"
+    : "bg-transparent backdrop-blur-sm";
 
   return createPortal(
     <>
       <section
-        className={
-          "absolute  top-0 right-0 h-screen w-screen bg-transparent backdrop-blur-sm transition-opacity ease-in duration-200 " +
-          opacityValue
-        }
+        aria-label="backdrop"
+        className={`absolute  top-0 right-0 h-screen w-screen transition-opacity ease-in duration-200 ${blurOrBackdrop} ${opacityValue}`}
         onClick={onClose}
       ></section>
       <section
-        className={
-          "absolute bg-gray-700 rounded-lg text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-500 p-3 backdrop-blur-3xl  transition-opacity ease-in duration-200 " +
-          opacityValue
-        }
+        aria-label="modal body"
+        className={`absolute bg-gray-700 rounded-lg text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-500 p-3 backdrop-blur-3xl  transition-opacity ease-in duration-200 ${opacityValue} ${className}`}
       >
         {children}
       </section>
