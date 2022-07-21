ce8bdbbb56aad9779bf870898d63deab721ce997
lakardion <lakardion@protonmail.com>
2022-07-21 11:59:35 -0300

    feat(spinner) ✨ add backdrop spinner component

--- src/components/tw-spinner/index.tsx
+++ src/components/tw-spinner/index.tsx
@@ -3,13 +3,32 @@ import styles from "./tw-spinner.module.css";
 
 export const Spinner: FC<{
   sizeClassNames?: string;
-}> = ({ sizeClassNames = "w-8 h-8" }) => {
+  className?: string;
+}> = ({ sizeClassNames = "w-8 h-8", className = "" }) => {
   return (
-    <div className="flex justify-center items-center">
+    <div
+      className={`${styles["spinner-border"]} animate-spin inline-block border-4 rounded-full ${sizeClassNames} ${className}`}
+      role="status"
+    ></div>
+  );
+};
+
+/**
+ * The parent of this component should be position-relative otherwise this is not going to work properly. It is based on the position relative of its parent so that it fills all the parent's space to position the loader and backdrop
+ * @returns
+ */
+export const BackdropSpinner: FC<{
+  isLoading: boolean;
+  spinnerSizeClassNames?: string;
+}> = ({ isLoading, spinnerSizeClassNames = "w-8 h-8" }) => {
+  if (!isLoading) return null;
+  return (
+    <div className="absolute w-full h-full flex justify-center items-center">
       <div
-        className={`${styles["spinner-border"]} animate-spin inline-block border-4 rounded-full ${sizeClassNames}`}
-        role="status"
+        className="w-full h-full bg-gray-300"
+        style={{ opacity: "50%" }}
       ></div>
+      <Spinner className="absolute" sizeClassNames={spinnerSizeClassNames} />
     </div>
   );
 };
