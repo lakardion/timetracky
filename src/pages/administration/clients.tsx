aae5d6aabd02672de1716eb7bbb7a023bc535faa
lakardion <lakardion@protonmail.com>
2022-07-22 09:14:48 -0300

    build 👷 change build script to run prisma cmds

--- src/pages/administration/clients.tsx
+++ src/pages/administration/clients.tsx
@@ -8,6 +8,8 @@ import { FC, useState } from "react";
 import { useForm } from "react-hook-form";
 import { trpc } from "utils/trpc";
 import { z } from "zod";
+import { MdDeleteOutline } from "react-icons/md";
+import { ConfirmForm } from "components/confirm-form";
 
 type Inputs = {
   name: string;
@@ -16,18 +18,16 @@ const clientEditCreateValidator = z.object({
   name: z.string().min(1, "Required"),
 });
 const ClientEditCreateForm: FC<{
+  clientId?: string;
   onFinish: () => void;
-  onSubmit: (data: Inputs) => void;
-}> = ({ onFinish }) => {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<Inputs>({
-    resolver: zodResolver(clientEditCreateValidator),
-  });
+}> = ({ onFinish, clientId = "" }) => {
   const queryInvalidator = trpc.useContext();
-  const { isLoading, mutateAsync } = trpc.useMutation(
+  const { data: client } = trpc.useQuery(
+    ["timetracky.getClient", { clientId: clientId }],
+    { enabled: Boolean(clientId) }
+  );
+
+  const { isLoading, mutateAsync: createClient } = trpc.useMutation(
     "timetracky.createClient",
     {
       onSuccess: () => {
@@ -36,14 +36,37 @@ const ClientEditCreateForm: FC<{
     }
   );
 
+  const { isLoading: isEditClientLoading, mutateAsync: updateClient } =
+    trpc.useMutation("timetracky.updateClient", {
+      onSuccess: () => {
+        queryInvalidator.invalidateQueries(["timetracky.clients"]);
+      },
+    });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: zodResolver(clientEditCreateValidator),
+    defaultValues: {
+      name: client?.name,
+    },
+  });
+
   const onSubmit = async (data: Inputs) => {
-    const createdClient = await mutateAsync({ name: data.name });
+    const createdClient = client?.id
+      ? await updateClient({ id: client.id, name: data.name })
+      : await createClient({ name: data.name });
     onFinish();
   };
 
+  const formTitle = clientId ? "Edit client" : "Create client";
+  const buttonLabel = clientId ? "Edit" : "Create";
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2 p-4">
-      <h1 className="text-3xl font-semibold">Create a client</h1>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 p-4">
+      <h1 className="text-3xl font-semibold">{formTitle}</h1>
       <label htmlFor="name" className="font-medium">
         Name
       </label>
@@ -59,7 +82,7 @@ const ClientEditCreateForm: FC<{
           isLoading={isLoading}
           disabled={isLoading}
         >
-          Create
+          {buttonLabel}
         </Button>
         <Button className="flex-grow" onClick={onFinish} disabled={isLoading}>
           Cancel
@@ -69,33 +92,69 @@ const ClientEditCreateForm: FC<{
   );
 };
 
-const ClientList = () => {
+const ClientList: FC<{
+  onClientClick: (clientId: string) => void;
+  onClientDelete: (clientId: string) => void;
+}> = ({ onClientClick, onClientDelete }) => {
   const { data: clients, isLoading } = trpc.useQuery(["timetracky.clients"], {
     keepPreviousData: true,
   });
 
+  const [hoveringId, sethoveringId] = useState("");
+  const createHoveringHandler = (id: string) => () => {
+    sethoveringId(id);
+  };
+  const removeHover = () => {
+    sethoveringId("");
+  };
   if (!clients && isLoading) {
     return <Spinner />;
   }
   if (!clients?.length) {
-    return <p className="text-xl">No clients to show</p>;
+    return (
+      <p className="text-base text-center italic pt-3">No clients to show</p>
+    );
   }
+  const createClientClickHandler = (id: string) => () => onClientClick(id);
+  const createClientDeleteHandler = (id: string) => () => onClientDelete(id);
+
   return (
-    <ul className="flex flex-col gap-3">
+    <ul className="flex flex-col gap-3 justify-center items-center">
       {clients.map((c) => {
         const projectsCount = c.projects?.length ?? 0;
         const projectPlural = "project" + (projectsCount > 1 ? "s" : "");
         return (
           <li
             key={c.id}
-            className="p-4 border border-solid border-gray-600/50 rounded hover:border-orange-400 flex justify-between"
+            className={`border border-solid border-gray-600/50 rounded hover:border-orange-400 w-full sm:max-w-2xl relative ${
+              c.isActive ? "" : "opacity-50"
+            }`}
+            onMouseEnter={createHoveringHandler(c.id)}
+            onMouseLeave={removeHover}
           >
-            <div className="font-semibold">{c.name}</div>
-            <div>
-              {c.projects?.length
-                ? `${c.projects.length} ${projectPlural}`
-                : "No projects for this client"}
-            </div>
+            <button
+              className="flex justify-between w-full p-4"
+              type="button"
+              onClick={createClientClickHandler(c.id)}
+            >
+              <p className="font-semibold">
+                {c.name}
+                {c.isActive ? "" : <span className="italic"> (Inactive)</span>}
+              </p>
+              <p>
+                {c.projects?.length
+                  ? `${c.projects.length} ${projectPlural}`
+                  : "No projects for this client"}
+              </p>
+            </button>
+            <button
+              className={`${
+                hoveringId === c.id ? "" : "hidden"
+              } absolute bg-red-300 border-2 border-solid border-gray-400 rounded-full p-1 -top-3 -right-3`}
+              onClick={createClientDeleteHandler(c.id)}
+            >
+              <MdDeleteOutline size={20} />
+            </button>
           </li>
         );
       })}
@@ -105,13 +164,50 @@ const ClientList = () => {
 
 const Clients = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [currentClientId, setCurrentClientId] = useState("");
+  const { data: client } = trpc.useQuery(
+    ["timetracky.getClient", { clientId: currentClientId }],
+    {
+      enabled: Boolean(currentClientId),
+    }
+  );
+  const queryClient = trpc.useContext();
+  const { isLoading: isDeleting, mutateAsync: deleteClient } = trpc.useMutation(
+    "timetracky.deleteClient",
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["timetracky.clients"]);
+      },
+    }
+  );
   const handleClose = () => {
     setIsOpen(false);
+    setCurrentClientId("");
   };
   const handleOpen = () => {
     setIsOpen(true);
   };
-  const handleSubmit = () => {};
+  const handleClientClick = (clientId: string) => {
+    setCurrentClientId(clientId);
+    setIsOpen(true);
+  };
+
+  const handleClientDelete = (clientId: string) => {
+    setCurrentClientId(clientId);
+    setIsConfirmModalOpen(true);
+  };
+  const handleConfirmDelete = async () => {
+    await deleteClient({ clientId: currentClientId });
+    setIsConfirmModalOpen(false);
+    setCurrentClientId("");
+  };
+  const clientIsLoading = Boolean(currentClientId && !client);
+
+  const handleCancelDelete = () => {
+    setCurrentClientId("");
+    setIsConfirmModalOpen(false);
+  };
 
   return (
     <>
@@ -123,13 +219,26 @@ const Clients = () => {
         <Button onClick={handleOpen}>Add a client</Button>
       </section>
       <section className="pt-2">
-        <ClientList />
+        <ClientList
+          onClientClick={handleClientClick}
+          onClientDelete={handleClientDelete}
+        />
       </section>
-      {isOpen ? (
-        <Modal onClose={handleClose}>
+      {isOpen && !clientIsLoading ? (
+        <Modal onBackdropClick={handleClose}>
           <ClientEditCreateForm
             onFinish={handleClose}
-            onSubmit={handleSubmit}
+            clientId={currentClientId}
+          />
+        </Modal>
+      ) : null}
+      {isConfirmModalOpen ? (
+        <Modal onBackdropClick={handleClose}>
+          <ConfirmForm
+            onConfirm={handleConfirmDelete}
+            onCancel={handleCancelDelete}
+            isConfirming={isDeleting}
+            body="Confirm deleting client"
           />
         </Modal>
       ) : null}
